6b98fd024a097d913cd531e06e235ef2e5968647
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-01-25 17:46:11 -0300

    fix deploy git url

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :user                    , 'deploy'
 set :application             , 'campfire'
 set :deploy_to               , "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :scm                     , :git
-set :repo_url                , 'git@example.com:username/appname.git'
+set :repo_url                , 'git@github.com:jCrip/campfire.git'
 set :branch                  , :master
 set :keep_releases           , 5
 set :ssh_options             , { forward_agent: true, user: fetch(:user) , keys: %w(~/.ssh/id_rsa.pub) }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :rails_env, :production
 
-server 'your_server_ip', port: your_port_num, roles: %w{web app db}, primary: true
+server '162.243.18.47', port: 22, roles: %w{web app db}, primary: true
