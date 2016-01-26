74483aefc4a1dfdb0a688185dd7d481f13f1903b
Juan Cristobal Pazos <juancri@lalegion.cl>
2016-01-26 14:26:45 -0300

    change action cable url on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'ws://jcri.cl/cable/'
+  config.action_cable.url = '/cable'
   config.action_cable.allowed_request_origins = [ 'http://jcri.cl', /http:\/\/jcri.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
